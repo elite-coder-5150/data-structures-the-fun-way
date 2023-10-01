b44d93c5f775131d2dfecd2e4758a494d8d4cbd5
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-09-30 23:42:41 -0500

    defined the find max jumps algortihm

---
+++ algorithms/array/find-max-jumps.js
@@ -0,0 +1,24 @@
+import {jump} from "../misc/search";
+
+const findMaxJumps = (arr, n) => {
+    let dp = new Array(n).fill(0);
+    let ans = '';
+
+    for (let i = n - 1; i >= 0; i--) {
+        dp[i] = arr[i];
+        let j = i + arr[i];
+
+        if (j < n) {
+            dp[i] = dp[i] + dp[j];
+        }
+
+        ans = Math.max(ans, dp[i]);
+    }
+
+    document.write(ans);
+}
+
+// driver code
+let  arr = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9];
+let n = arr.length;
+findMaxJumps(arr, n);
