af79f5282f0edf86546ac01a25e124a994915fa4
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-10-04 11:19:47 -0500

    fixed the bug in the reverse algorithm

--- algorithms/math/get-sum.js
+++ algorithms/math/get-sum.js
@@ -1,4 +1,4 @@
-const reverse = (n) => {
+ const reverse = (n) => {
     let rev = 0;
 
     while (n !==  0) {
@@ -7,15 +7,31 @@ const reverse = (n) => {
     }
 
     return rev;
-}
+ }
 
+ /**
+  * function to find the sum of the odd and even
+  * positioned digits in a number
+  */
 const getSum = (n) => {
-    let sum = 0;
+    n = reverse(n);
+    let sumOdd = 0, sumEven = 0, c = 1;
 
     while (n !== 0) {
-        sum += n % 10;
-        n = Math.floor(n / 10);
-    }
+        if (c % 2 == 0) {
+            sumEven += n % 10;
+        } else {
+            sumOdd += n % 10;
+        }
 
-    return sum;
+        n = math.floor(n / 10);
+        c++;
+    }
+    
+    document.write("Sum odd = " + sumOdd);
+    document.write("<br />");
+    document.write("Sum even = " + sumEven);
 }
+
+let n = 457892;
+getSum(n);ß
