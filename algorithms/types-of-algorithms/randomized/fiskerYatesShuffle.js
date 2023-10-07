1db94e9de8ca88cc2c10e36f2ea85014b24908de
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-10-07 16:38:45 -0500

    defined the fisker yates shuffle algorithm

--- algorithms/types-of-algorithms/randomized/fiskerYatesShuffle.js
+++ algorithms/types-of-algorithms/randomized/fiskerYatesShuffle.js
@@ -1,4 +1,4 @@
-const fisherYatesShuffle = (arr) => {
+const fiskerYatesShuffle = (arr) => {
     const shuffledArr = arr.slice();
 
     for (let i = shuffledArr.length - 1; i > 0; i--) {
@@ -13,5 +13,5 @@ const fisherYatesShuffle = (arr) => {
 }
 
 const arr = [1,2,3,4,5];
-const sArr = fisherYatesShuffle(arr);
+const sArr = fiskerYatesShuffle(arr);
 console.log(sArr); // outputs a random permutation of the original array.
