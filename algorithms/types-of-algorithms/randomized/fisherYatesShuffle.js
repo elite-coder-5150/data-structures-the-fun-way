2909c1bb88a429864639bfe267b23046dba39ed7
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-10-07 16:34:01 -0500

    implemented the fisker-yates shuffle algorithm

---
+++ algorithms/types-of-algorithms/randomized/fisherYatesShuffle.js
@@ -0,0 +1,17 @@
+const fisherYatesShuffle = (arr) => {
+    const shuffledArr = arr.slice();
+
+    for (let i = shuffledArr.length - 1; i > 0; i--) {
+         const randomIdx =  Math.floor(Math.random() * (i + 1));
+
+         const temp = shuffledArr[i];
+         shuffledArr[i] = shuffledArr[randomIdx];
+         shuffledArr[randomIdx] = temp;
+    }
+
+    return shuffledArr
+}
+
+const arr = [1,2,3,4,5];
+const sArr = fisherYatesShuffle(arr);
+console.log(sArr); // outputs a random permutation of the original array.
